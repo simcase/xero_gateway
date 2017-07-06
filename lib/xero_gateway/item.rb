0b6cc346efe3d18e74a68da6bbd79663e55347af
Oleg Zotov <olegzotov73@gmail.com>
2017-07-06 18:10:22 +0400

    restore old item code

--- lib/xero_gateway/item.rb
+++ lib/xero_gateway/item.rb
@@ -1,27 +1,41 @@
 module XeroGateway
-  class Item < BaseRecord
-    attributes({
-        "ItemID" => :string,
-        "Code"          => :string,
-        "InventoryAssetAccountCode" => :string,
-        "Name"          => :string,
-        "IsSold"        => :boolean,
-        "IsPurchased"   => :boolean,
-        "Description"   => :string,
-        "PurchaseDescription" => :string,
-        "IsTrackedAsInventory" => :boolean,
-        "TotalCostPool" => :float,
-        "QuantityOnHand" => :integer,
+  class Item
 
-        "SalesDetails"  => {
-          "UnitPrice"     => :float,
-          "AccountCode"   => :string
-        },
+    attr_accessor :code, :name, :description, :sales_details
 
-        "PurchaseDetails" => {
-          "UnitPrice"     => :float,
-          "AccountCode"   => :string
+    def initialize(params = {})
+      params.each do |k,v|
+        self.send("#{k}=", v)
+      end
+    end
+
+    def to_xml(b = Builder::XmlMarkup.new)
+      b.Item {
+        b.Code code
+        b.Name name
+        b.Description description
+        b.SalesDetails {
+          b.UnitPrice sales_details[:unit_price]
+          b.AccountCode sales_details[:account_code]
         }
-    })
+      }
+    end
+
+    def self.from_xml(item_element)
+      item = Item.new
+      item.sales_details = {}
+      item_element.children.each do |element|
+        case(element.name)
+          when "Code" then item.code = element.text
+          when "Name" then item.name = element.text
+          when "Description" then item.description = element.text
+          when "SalesDetails" then element.children.each do |e|
+            item.sales_details[e.name.underscore.to_sym] = e.text
+          end
+        end
+      end
+      item
+    end
+
   end
 end
